da4900b79f741cf18b4f49b660628f28206792f6
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-03-25 14:46:33 +0500

    Add project files.

---
+++ 2.0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int number;
+        while (!int.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine("Enter only an integer number, please! ");
+            Console.Write("Enter a number: ");
+        }
+
+        int factorial = 1;
+        for (int i = 1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+        Console.WriteLine($"Factorial of {number}: {factorial}");
+    }
+}
